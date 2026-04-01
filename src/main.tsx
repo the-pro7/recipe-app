a02a44357b0a3a90e02b2bbec44f938075a0a960
the-pro7 <ameyawemmauel900@gmail.com>
2026-04-01 15:14:51 +0000

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 
-createRoot(document.getElementById('root')!).render(
+// const rootRoute = createRootRoute({
+//   component: () => (
+//     <div>
+//       <h1>Hello</h1>
+//       <Link to="/">Home</Link>
+//       <Outlet />
+//     </div>
+//   ),
+// });
+
+// const indexRoute = createRoute({
+//   getParentRoute: () => rootRoute,
+//   path: "/",
+//   component: function Index() {
+//     return <h1>Hello "/"!</h1>;
+//   },
+// });
+
+// const cuisineRoute = createRoute({
+//   getParentRoute: () => rootRoute,
+//   path: "/cuisine",
+//   component: function Cuisine() {
+//     return <h1>Hello "/cuisine"!</h1>;
+//   },
+// });
+
+// const routeTree = rootRoute.addChildren([indexRoute, cuisineRoute]);
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
